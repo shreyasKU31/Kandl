aaf3f8ef14b5bc1e4cc0e10d4112ba39329e866b
shreyasKU31 <shreyasku31@gmail.com>
2025-06-12 13:36:00 +0530

    fix

--- frontend/src/components/Signup/Investment.jsx
+++ frontend/src/components/Signup/Investment.jsx
@@ -9,30 +9,30 @@ const Investment = () => {
       </h2>
       <div className="grid grid-cols-2 gap-10 mb-8 m-auto">
         <div className="flex items-center gap-8">
-          <img src={assets.acopBenefits} alt="" className="w-45" />
+          <img src={assets.acopBenefits} alt="" className="w-40" />
           <div className="">
-            <h3 className="text-3xl mb-2">Stocks</h3>
+            <h3 className="text-2xl mb-2">Stocks</h3>
             <p>Invest in all exchange-listed securities</p>
           </div>
         </div>
         <div className="flex items-center gap-8">
-          <img src={assets.mfAcop} alt="" className="w-45" />
+          <img src={assets.mfAcop} alt="" className="w-40" />
           <div>
-            <h3 className="text-3xl mb-2">Mutual funds</h3>
+            <h3 className="text-2xl mb-2">Mutual funds</h3>
             <p>Invest in commission-free direct mutual funds</p>
           </div>
         </div>
         <div className="flex items-center gap-8">
-          <img src={assets.ipoAcop} alt="" className="w-45" />
+          <img src={assets.ipoAcop} alt="" className="w-40" />
           <div>
-            <h3 className="text-3xl mb-2">IPO</h3>
+            <h3 className="text-2xl mb-2">IPO</h3>
             <p>Apply to the latest IPOs instantly via UPI</p>
           </div>
         </div>
         <div className="flex items-center gap-8">
-          <img src={assets.foAcop} alt="" className="w-45" />
+          <img src={assets.foAcop} alt="" className="w-40" />
           <div>
-            <h3 className="text-3xl mb-2">Futures & options</h3>
+            <h3 className="text-2xl mb-2">Futures & options</h3>
             <p>Hedge and mitigate market risk through simplified F&O trading</p>
           </div>
         </div>
