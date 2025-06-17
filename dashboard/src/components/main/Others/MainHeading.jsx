bfd5eebfa8a342ac2286de076552d764913dcc3f
shreyasKU31 <shreyasku31@gmail.com>
2025-06-17 18:59:02 +0530

    add component MainHeading

---
+++ dashboard/src/components/main/Others/MainHeading.jsx
@@ -0,0 +1,9 @@
+const MainHeading = ({ text }) => {
+  return (
+    <div>
+      <h2 className="text-3xl font-medium py-2 mb-8 border-b-2">{text}</h2>
+    </div>
+  );
+};
+
+export default MainHeading;
