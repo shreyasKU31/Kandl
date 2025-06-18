51e0c328b45875a7917b9c84e791f917588153cd
shreyasKU31 <shreyasku31@gmail.com>
2025-06-18 08:50:59 +0530

    add holding component

---
+++ dashboard/src/components/main/holdings/Holding.jsx
@@ -0,0 +1,34 @@
+const Holding = ({ holding, id, price }) => {
+  let totalPrice = holding.qty * holding.avg;
+  let totalInvested = totalPrice + (totalPrice * parseInt(holding.net)) / 100;
+  return (
+    <div key={id} className="holdings">
+      <p className="text-left">{holding.name}</p>
+      <p>{holding.qty}</p>
+      <p className="max-lg:hidden">₹{holding.avg}</p>
+      <p>₹{holding.qty * holding.avg}</p>
+      <p
+        className={
+          holding.price < holding.avg
+            ? "text-red-400 max-lg:hidden"
+            : "text-green-500 max-lg:hidden"
+        }
+      >
+        {price ? holding.net : `₹${totalInvested}`}
+      </p>
+      <p
+        className={
+          holding.price < holding.avg ? "text-red-400" : "text-green-500"
+        }
+      >
+        ₹{holding.price}
+      </p>
+
+      <p className={holding.day < 0 ? "text-red-400" : "text-green-500"}>
+        {holding.day}
+      </p>
+    </div>
+  );
+};
+
+export default Holding;
