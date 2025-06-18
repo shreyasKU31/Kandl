6e61c6bb7799264029dc49c5de08e44a47bd09f7
shreyasKU31 <shreyasku31@gmail.com>
2025-06-18 08:44:17 +0530

    change padding

--- dashboard/src/components/main/Main.jsx
+++ dashboard/src/components/main/Main.jsx
@@ -9,7 +9,7 @@ import Apps from "./apps/Apps";
 
 const Main = () => {
   return (
-    <div className="bg-white/10 backdrop-blur-md shadow-2xl py-2 px-4 rounded-xl border border-white/20 h-full">
+    <div className="bg-white/10 backdrop-blur-md shadow-2xl py-2 px-8 rounded-xl border border-white/20 h-full">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/holding" element={<Holdings />} />
