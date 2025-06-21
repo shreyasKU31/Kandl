23f0c478216b6661a679f3f35945dc5ac5d28fc8
shreyasKU31 <shreyasku31@gmail.com>
2025-06-21 19:54:37 +0530

    connect DB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
 import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
 const app = express();
 const port = 4000;
+
+const connectDB = async () => {
+  await mongoose.connect("mongodb://127.0.0.1:27017/kandl");
+};
+connectDB()
+  .then(() => {
+    console.log("Database is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Server");
