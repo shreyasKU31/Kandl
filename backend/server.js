9612a283c82fb66d7e7a4775409bf718164f72ac
shreyasKU31 <shreyasku31@gmail.com>
2025-07-17 16:45:10 +0530

    impliment order route

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const port = 4000;
 import cors from "cors";
 import bodyParser from "body-parser";
 import watchList from "./model/Watchlist.js";
+import Order from "./model/Orders.js";
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -33,6 +34,15 @@ app.get("/holdings", async (req, res) => {
 app.get("/watchlist", async (req, res) => {
   const watchlist = await watchList.find({});
   res.send(watchlist);
+});
+app.get("/order", async (req, res) => {
+  const order = await Order.find({});
+  res.send(order);
+});
+
+app.post("/buy", async (req, res) => {
+  let data = req.body;
+  await Order.insertOne(data);
 });
 
 app.listen(port, () => {
